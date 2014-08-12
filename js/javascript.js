c38872efbe73f910be891b8bda4670159660bce7
Andrey Nozdrenko <anno@qapint.com>
2014-08-12 20:12:23 +0300

    add

---
+++ js/javascript.js
@@ -0,0 +1,74 @@
+function Monster(name, life){
+		this.name = name;
+		this.startLife = life; 
+		this.life = this.startLife;
+
+function catMonster(obj){
+		this.takenMonster.call(obj, 5);
+}
+
+function birdMonster(obj){
+		this.takenMonster.call(obj, 3);
+}
+
+	this.speacName = function(){
+		console.log(this.name);
+	}
+
+	this.takenMonster = function(countTaken){
+		this.life = this.life - countTaken;
+	}
+
+	this.getAttack  = function(obj){
+			if(name.indexOf("cat") != -1){
+				this.takenCatMonster = catMonster;
+				this.startLife = 50;
+				this.life = this.startLife;
+			}
+			if(name.indexOf("bird") != -1){
+				this.takenCatMonster = birdMonster;
+				this.startLife = 60;
+				this.life = this.startLife;
+			}
+	}
+}
+
+
+var catMon = new Monster("catMon",70),
+	catMon1 = new Monster("catMon1",70),
+	catMon2 = new Monster("catMon2",70),
+	catMon3 = new Monster("catMon3",70),
+	catMon4 = new Monster("catMon4",70),
+
+	birdMon = new Monster("birdMon",70),
+	birdMon1 = new Monster("birdMon1",70),
+	birdMon2 = new Monster("birdMon2",70),
+	birdMon3 = new Monster("birdMon3",70);
+
+	catMon.getAttack();
+	catMon1.getAttack();
+	catMon2.getAttack();
+	catMon3.getAttack();
+	catMon4.getAttack();
+
+	birdMon.getAttack();
+	birdMon1.getAttack();
+	birdMon2.getAttack();
+	birdMon3.getAttack();
+
+	catMon.speacName();
+	catMon1.speacName();
+
+	catMon.takenCatMonster(catMon1);
+
+	console.log(catMon.life);
+	console.log(catMon1.life);
+
+	birdMon.takenCatMonster(catMon);
+	console.log(catMon.life);
+
+for(var i = 0; i < 7; i++){
+		catMon.takenCatMonster(birdMon2);
+}
+	console.log(birdMon2.life);
+	console.log(birdMon3.life);
